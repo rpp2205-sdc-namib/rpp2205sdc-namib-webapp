eb15e0b4b621bf12e5be9ef8afef20b23b66cfe7
bbissing <bbissing101@gmail.com>
2022-09-29 18:45:00 -0500

    Added skeletons for all our routes. Changed port from 3001 to 3000.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+//get routes
+app.get('/products', (req, res) => {
+
+});
+
+app.get('/reviews', (req, res) => {
+
+});
+
+app.get('/qa/questions', (req, res) => {
+
+});
+
+
+//post routes
+app.post('/products', (req, res) => {
+
+});
+
+app.post('/reviews', (req, res) => {
+
+});
+
+app.post('/qa/questions', (req, res) => {
+
+});
+
+
+//put routes
+app.put('/products', (req, res) => {
+
+});
+
+app.put('/reviews', (req, res) => {
+
+});
+
+app.put('/qa/questions', (req, res) => {
+
+});
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on  http://localhost:${port}`);
+  console.log(`Listening on  http://localhost:${port}`);
 });
