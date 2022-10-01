23e91f20535078475692e7d36f97b862cddb064e
bbissing <bbissing101@gmail.com>
2022-10-01 16:02:46 -0500

    test

---
+++ server/controller/helper.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const axios = require('axios');
+const API_Link = `https://app-hrsei-api.herokuapp.com/api/fec2/${process.env.CAMPUS_CODE}`;
+
+module.exports = {
+
+  getProductHandler: (req, res) => {
+    var product_id = req.params.product_id;
+    axios.get(`${API_Link}/products/${product_id}`, {headers: {Authorization: process.env.access_token}})
+      .then(response => {
+      console.log(response.data);
+      res.status(200).send(response.data)
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send(err);
+      })
+
+  },
+
+  getReviewsHandler: (req, res) => {
+    var param = req.params.param;
+    var value = req.params.value;
+    axios.get(`${API_Link}/reviews?product_id=${value}`, {headers: {Authorization: process.env.access_token}})
+      .then(response => {res.status(200).send(response.data)})
+      .catch(err => {console.error(err); res.status(500).send(err)});
+  },
+
+  getQuestionsHandler: (req, res) => {
+    var product_id = req.params.product_id;
+
+    axios.get(`${API_Link}/qa/questions?product_id=${product_id}`, {
+      headers: {
+        'Authorization': process.env.access_token
+      }
+    })
+    .then(response => {
+      console.log('response: ', response);
+      res.status(200).send(response.data);
+    })
+    .catch(err => {
+      res.sendStatus(500);
+    })
+  },
+
+  postReviewHandler: (req, res) => {
+    var { rating, recommend, characteristics, summay, body } = req.body;
+    axios.post(`${API_Link}/reviews`, {product_id, rating, summary, })
+  },
+
+  // postQuestionsHandler: (req, res) => {
+
+  // }
+
+};
