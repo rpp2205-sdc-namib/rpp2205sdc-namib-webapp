b85d08135c7ad8ba17bacd240eab1e8cd9f5716c
Sijia Tao <taoshika127@gmail.com>
2022-10-03 20:33:27 -0700

    add fivestars component that can potentially be reused

---
+++ client/src/components/FiveStars.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Stars extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>
+
+    </div>)
+
+  }
+}
+
+export default Stars;
