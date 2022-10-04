ee82c160b7f392f1bad2dee6b41de80e75997873
Sijia Tao <taoshika127@gmail.com>
2022-10-03 20:30:57 -0700

    add state and change state function in App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,12 +5,17 @@ import Overview from './Overview/overview.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {currentProductID: ''};
+    this.state = {currentProductId: ''};
+  }
+
+  handleProductIdChange(newId) {
+    //can be used by all components for product ID change
+    this.setState({currentProductId: newId})
   }
 
   render() {
     return <div>
-      <Overview />
+      <Overview productId={this.state.currentProductId} handleProductIdChange={this.handleProductIdChange.bind(this)}/>
     </div>
   }
 }
