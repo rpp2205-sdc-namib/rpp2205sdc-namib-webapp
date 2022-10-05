9cdd9b6a1ed9139feb492e5f54a95e1334f3a2ab
Yui1002 <yuimurayama1002@gmail.com>
2022-10-05 00:01:23 -0700

    save current work

---
+++ client/src/components/RelatedItems_Comparison/Answers.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Answers extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Answers</div>
+    )
+  }
+}
+
+export default Answers;
---
+++ client/src/components/RelatedItems_Comparison/Questions.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Questions extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Questions</div>
+    )
+  }
+}
+
+export default Questions;
