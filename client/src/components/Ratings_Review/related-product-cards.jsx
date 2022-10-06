92d03477f6503b93301a0107a64e8f1baf7fe5c9
Stacey Pereira <staceypereira1@gmail.com>
2022-10-05 18:10:38 -0700

    Skeleton for component files

---
+++ client/src/components/Ratings_Review/related-product-cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class RPC extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return(
+
+      )
+  }
+
+}
+
+export default RPC;
---
+++ client/src/components/Ratings_Review/rp-list.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class rpList extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+
+  }
+
+}
+
+export default rpList;
