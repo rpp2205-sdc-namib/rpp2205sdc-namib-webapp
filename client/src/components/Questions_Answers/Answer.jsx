4c328668ae4dc759d113f4ddf76623e800c1c321
Yui1002 <yuimurayama1002@gmail.com>
2022-10-10 12:36:34 -0700

    added a feature to sort answers by seller or not

--- client/src/components/Questions_Answers/Answer.jsx
+++ client/src/components/Questions_Answers/Answer.jsx
@@ -4,38 +4,52 @@ import React from 'react';
 class Answer extends React.Component {
   constructor(props) {
     super(props);
+    console.log('props in answer: ', props)
     this.state = {
       helpfulness: props.answer.helpfulness,
+      isYesClicked: false,
       isReported: false
     }
 
     this.handleReport = this.handleReport.bind(this);
     this.handleIncreaseCounts = this.handleIncreaseCounts.bind(this);
+    this.handleDateFormat = this.handleDateFormat.bind(this);
   }
 
   handleReport() {
     // change the text to "Reported"
     this.setState({
-      isReported: !this.state.isReported
+      isReported: true
     });
   }
 
   handleIncreaseCounts() {
     // increase the count of helpfulness
     this.setState({
+      isYesClicked: true,
       helpfulness: this.state.helpfulness + 1
     });
+  }
+
+  handleDateFormat(date) {
+    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    let d = new Date(date);
+    let day = d.getDate();
+    let month = monthNames[d.getMonth()];
+    let year = d.getFullYear();
+    return `${month} ${day}, ${year}`;
   }
 
   render() {
     return (
       <div>
         <p>A: {this.props.answer.body}</p>
-        <div>by User: {this.props.answer.answerer_name}</div>
-        <div>date: {this.props.answer.date}</div>
+        <div>by {this.props.answer.answer_name === "Seller" ? "Seller" : this.props.answer.answerer_name}</div>
+        <div>{this.handleDateFormat(this.props.answer.date)}</div>
         <div>Helpful?</div>
-        <div onClick={this.handleIncreaseCounts}>Yes<span>{this.state.helpfulness}</span></div>
-        {this.state.isReported ? <div onClick={this.handleReport}>Reported</div> : <div onClick={this.handleReport}>Report</div>}
+        <button disabled={this.state.isYesClicked} onClick={this.handleIncreaseCounts}>Yes<span>{this.state.helpfulness}</span></button>
+        {this.state.isReported ? <button disabled={this.state.isReported} onClick={this.handleReport}>Reported</button> : <button onClick={this.handleReport}>Report</button>}
       </div>
     )
   }
--- client/src/components/Questions_Answers/Answers.jsx
+++ client/src/components/Questions_Answers/Answers.jsx
@@ -4,13 +4,39 @@ import Answer from './Answer.jsx';
 class Answers extends React.Component {
   constructor(props) {
     super(props);
+
+    this.sortAnswers = this.sortAnswers.bind(this);
+    this.sortBySeller = this.sortBySeller.bind(this);
+  }
+
+  sortAnswers(callback, index) {
+    let results = this.props[index]
+    .filter(callback)
+    .sort((a, b) => {
+      return b.helpfulness - a.helpfulness;
+    });
+
+    return results;
+  }
+
+  sortBySeller(index) {
+    let sellerSorted = this.sortAnswers((answer) => {
+      return answer.answerer_name === 'Seller';
+    }, index);
+
+    let notSellerSorted = this.sortAnswers((answer) => {
+      return answer.answerer_name !== 'Seller';
+    }, index);
+
+    let results = sellerSorted.concat(notSellerSorted);
+    return results;
   }
 
   render() {
     if (this.props.allAnswersForFirstQuestion) {
       return (
         <div>
-          {this.props.allAnswersForFirstQuestion.map((answer, index) => {
+          {this.sortBySeller('allAnswersForFirstQuestion').map((answer, index) => {
             if (index > 1) return;
             return (
               <div key={answer.answer_id}>
@@ -23,7 +49,7 @@ class Answers extends React.Component {
     } else {
       return (
         <div>
-          {this.props.allAnswersForSecondQuestion.map((answer, index) => {
+          {this.sortBySeller('allAnswersForSecondQuestion').map((answer, index) => {
             if (index > 1) return;
             return (
               <div key={answer.answer_id}>
