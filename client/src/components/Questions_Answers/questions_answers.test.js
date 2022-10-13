18d9b97f58c18d9e4e694b5c6d0b46e13417e998
Yui1002 <yuimurayama1002@gmail.com>
2022-10-12 19:07:28 -0700

    added test case to check if the component renders correctly

--- client/src/components/Questions_Answers/questions_answers.test.js
+++ client/src/components/Questions_Answers/questions_answers.test.js
@@ -1,4 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import ModalWindow from './ModalWindow.jsx';
+import Questions_Answers from './Questions_Answers.jsx';
+
+/**
+ * @jest-environment jsdom
+ */
+
+describe('component rendering', function() {
+  test("should render Questions_Answers component correctly", function() {
+    render(<Questions_Answers />);
+    screen.debug();
+  })
+})
+
 
 describe('Email validation', function() {
   test("should validate as a invalid email address", function() {
@@ -11,7 +26,7 @@ describe('Email validation', function() {
     expect(modalInstance.validateEmail(invalidInput2)).toBeNull();
     expect(modalInstance.validateEmail(invalidInput3)).toBeNull();
   });
-  test("should validate as a valie email address", function() {
+  test("should validate as a valid email address", function() {
     let modalInstance = new ModalWindow();
     let validInput = 'jack@gmail.com';
     let validInput2 = 'jack@email.com'
