27266144b7782d5d10d3a03ea46cd0c9e071b768
bbissing <bbissing101@gmail.com>
2022-10-17 22:37:22 -0500

    Added a modal for my Reviews Component to help expand the photos that reviewers submitted. Changed the CSS on my Reviews Component to make it look a little more like the wireframe example that was given to us. Added the “I recommend this product” checkmark for each tile whenever a reviewer recommends the product. Added a “Helpful” response for each tile (or review). This helps illustrate how many users found this specific review helpful. The user can also cast a vote. It automatically disables once a vote has been casted. Added progress bars for my reviews breakdown sub-component to help track how many specific star ratings (5, 4, 3, 2, 1) did a specific product receive. Added another state parameter to App.jsx to pass down totalRatings. Needed this for my ratings breakdown subcomponent.

--- client/src/components/Ratings_Reviews/Rating_Breakdown.jsx
+++ client/src/components/Ratings_Reviews/Rating_Breakdown.jsx
@@ -4,17 +4,93 @@ import Stars from '../FiveStars.jsx';
 class Rating_Breakdown extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      fiveStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['5']),
+      fourStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['4']),
+      threeStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['3']),
+      twoStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['2']),
+      oneStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['1'])
+    };
+
+    this.percentageOfTotalReviews = this.percentageOfTotalReviews.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.productId !== prevProps.productId) {
+      this.setState({
+        fiveStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['5']),
+        fourStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['4']),
+        threeStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['3']),
+        twoStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['2']),
+        oneStarReviews: this.percentageOfTotalReviews(this.props.totalRatings, this.props.ratings['1'])
+      })
+    }
+  }
+
+  percentageOfTotalReviews(totalRatings, totalSpecificRatings) {
+    const percentageOfTotalSpecificRatings = `${parseFloat(totalSpecificRatings/totalRatings).toFixed(2)*100}%`
+    console.log('Ratings_Breakdown.jsx - percentageOfTotalReviews() - totalRatings', totalRatings);
+    console.log('Ratings_Breakdown.jsx - percentageOfTotalReviews() - data3', percentageOfTotalSpecificRatings);
+    return percentageOfTotalSpecificRatings;
   }
 
   render() {
+    console.log(this.props.ratings);
+    console.log(this.state.fiveStarReviews);
+    console.log(this.props.productId);
     return (
-      <div>
-        <div>Average rating for one specific product.</div>
+      <div className="ratings_breakdown">
+        <strong>Ratings and Reviews</strong>
         <div data-testid="star_rating">{this.props.rating}</div>
         <Stars rating={this.props.rating}/>
-        <div>Total Reviews: {this.props.totalReviews}</div>
-        <div><hr /></div>
+        <div>Total Ratings: {this.props.totalRatings}</div>
+        <div className="progress_bars">
+          <a className="star_progress_bar" onClick={() => {this.props.filterReviews()}}>
+            <div style={{float:'left'}}>5 Stars</div>
+            <div className="progress">
+              <div className="bar" style={{width:`${this.state.fiveStarReviews}`}}>
+                <p className="percent">{this.state.fiveStarReviews}</p>
+              </div>
+            </div>
+            <div style={{float:'left'}}>{this.props.ratings['5']}</div>
+          </a>
+          <a className="star_progress_bar" onClick={(e) => {this.props.filterReviews(e)}}>
+            <div style={{float:'left'}}>4 Stars</div>
+            <div className="progress">
+              <div className="bar" style={{width:`${this.state.fourStarReviews}`}}>
+                <p className="percent">{this.state.fourStarReviews}</p>
+              </div>
+            </div>
+            <div style={{float:'left'}}>{this.props.ratings['4']}</div>
+          </a>
+          <a className="star_progress_bar" onClick={() => {this.props.filterReviews()}}>
+            <div style={{float:'left'}}>3 Stars</div>
+            <div className="progress">
+              <div className="bar" style={{width:`${this.state.threeStarReviews}`}}>
+                <p className="percent">{this.state.threeStarReviews}</p>
+              </div>
+            </div>
+            <div style={{float:'left'}}>{this.props.ratings['3']}</div>
+          </a>
+          <a className="star_progress_bar" onClick={() => {this.props.filterReviews()}}>
+            <div style={{float:'left'}}>2 Stars</div>
+            <div className="progress">
+              <div className="bar" style={{width:`${this.state.twoStarReviews}`}}>
+                <p className="percent">{this.state.twoStarReviews}</p>
+              </div>
+            </div>
+            <div style={{float:'left'}}>{this.props.ratings['2']}</div>
+          </a>
+          <a className="star_progress_bar" onClick={() => {this.props.filterReviews()}}>
+            <div style={{float:'left'}}>1 Stars</div>
+            <div className="progress">
+              <div className="bar" style={{width:`${this.state.oneStarReviews}`}}>
+                <p className="percent">{this.state.oneStarReviews}</p>
+              </div>
+            </div>
+            <div style={{float:'left'}}>{this.props.ratings['1']}</div>
+          </a>
+        </div>
       </div>
     )
   }
--- client/src/components/Ratings_Reviews/Ratings_Reviews.jsx
+++ client/src/components/Ratings_Reviews/Ratings_Reviews.jsx
@@ -7,12 +7,18 @@ class Ratings_Reviews extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.filterReviews = this.filterReviews.bind(this);
+  }
+
+  filterReviews(e) {
+    console.log('Ratings_Reviews.jsx - filterReviews() has been called', e);
   }
 
   render() {
     return (
-      <div>
-        <Rating_Breakdown productId={this.props.productId} rating={this.props.rating} totalReviews={this.props.totalReviews}/>
+      <div className="review_container">
+        <Rating_Breakdown productId={this.props.productId} rating={this.props.rating} ratings={this.props.ratings} totalReviews={this.props.totalReviews} totalRatings={this.props.totalRatings} filterReviews={this.filterReviews}/>
         <Reviews_List productId={this.props.productId} reviews={this.props.reviews} totalReviews={this.props.totalReviews}/>
       </div>
     )
