dd8a6fac583c299be41365449373b6162bc0fad7
bbissing <bbissing101@gmail.com>
2022-10-19 00:12:14 -0500

    User can click on ratings bar to filter reviews A clear button is provided to clear all filters Messages are provided to let the user know which filters on on

--- client/src/components/Ratings_Reviews/Ratings_Reviews.jsx
+++ client/src/components/Ratings_Reviews/Ratings_Reviews.jsx
@@ -8,19 +8,27 @@ class Ratings_Reviews extends React.Component {
     super(props);
     this.state = {
       filteredReviews: [],
-      isToggled: false,
-      value: null
+      isToggled: [0,0,0,0,0]
     };
 
+    this.clearFilter = this.clearFilter.bind(this);
     this.filterReviews = this.filterReviews.bind(this);
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.productId !== prevProps.productId) {
       this.setState({
-        filteredReviews: []
+        filteredReviews: [],
+        isToggled: [0,0,0,0,0]
       })
     }
+  }
+
+  clearFilter() {
+    this.setState({
+      filteredReviews: [],
+      isToggled: [0,0,0,0,0]
+    })
   }
 
   filterReviews(e) {
@@ -28,32 +36,33 @@ class Ratings_Reviews extends React.Component {
     console.log(e.target.innerText[0]);
     const value = parseInt(e.target.innerText[0]);
     const reviews = this.props.reviews;
-    const results = reviews.filter(result => {
-      if (result.rating === parseInt(value)) {
+    let results = reviews.filter(result => {
+      if (result.rating === value) {
         return result;
       }
     });
 
-    if (this.state.isToggled && value === this.state.value) {
+    if (this.state.isToggled[value - 1] === 1) {
+      results = this.state.filteredReviews.filter(result => {
+        if (result.rating !== value) {
+          return result;
+        }
+      });
+      this.state.isToggled[value - 1] = 0;
       this.setState({
-        filteredReviews: [],
-        isToggled: false,
-        value: null
-      })
-    } else if (this.state.filteredReviews.length > 0 ){
-      const results2 = this.state.filteredReviews.concat(results);
-      console.log('testing 1 2 3');
+        filteredReviews: results
+      }, () => {console.log(this.state.filteredReviews)});
+    } else if (this.state.filteredReviews.length > 0 ) {
+      results = this.state.filteredReviews.concat(results);
+      this.state.isToggled[value - 1] = 1;
       this.setState({
-        filteredReviews: results2,
-        isToggled: true,
-        value: value
-      }, () => {console.log(this.state.filteredReviews)})
+        filteredReviews: results
+      }, () => {console.log(this.state.filteredReviews)});
     } else {
+      this.state.isToggled[value - 1] = 1;
       this.setState({
-        filteredReviews: results,
-        isToggled: true,
-        value: value
-      }, () => {console.log(this.state.filteredReviews)})
+        filteredReviews: results
+      }, () => {console.log(this.state.filteredReviews)});
     }
 
   }
@@ -61,7 +70,7 @@ class Ratings_Reviews extends React.Component {
   render() {
     return (
       <div className="review_container">
-        <Rating_Breakdown productId={this.props.productId} rating={this.props.rating} ratings={this.props.ratings} totalReviews={this.props.totalReviews} totalRatings={this.props.totalRatings} filterReviews={this.filterReviews}/>
+        <Rating_Breakdown productId={this.props.productId} rating={this.props.rating} reviews={this.props.reviews} ratings={this.props.ratings} totalReviews={this.props.totalReviews} totalRatings={this.props.totalRatings} filterReviews={this.filterReviews} clearFilter={this.clearFilter}/>
         <Reviews_List productId={this.props.productId} reviews={this.state.filteredReviews.length > 0 ? this.state.filteredReviews : this.props.reviews} totalReviews={this.state.filteredReviews.length > 0 ? this.state.filteredReviews.length : this.props.totalReviews}/>
       </div>
     )
