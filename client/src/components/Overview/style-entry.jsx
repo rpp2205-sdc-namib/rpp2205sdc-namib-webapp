d2cc29941c98a72c48b111495194701c569adc79
Sijia Tao <taoshika127@gmail.com>
2022-10-18 17:57:29 -0700

    add more css styling

--- client/src/components/Overview/style-entry.jsx
+++ client/src/components/Overview/style-entry.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const highlightStyle = {"borderStyle": "solid", "color": "orange", "borderWidth": "2.5px"};
+const highlightStyle = {"borderStyle": "solid", "color": "rgb(116, 196, 65)", "borderWidth": "2.5px"};
 const nonHighlightStyl = {"borderStyle": "none"};
+
 
 class StyleEntry extends React.Component {
   constructor(props) {
@@ -14,9 +15,15 @@ class StyleEntry extends React.Component {
   }
 
   render() {
-    return (<div className="styleThumbnails-container" style={this.props.highlight ? highlightStyle : nonHighlightStyl}>
-      <img className="styleThumbnails" src={this.props.styleObj.photos[0].thumbnail_url || 'img/NoImageThumbnail.png'} onClick={this.handleMouseOver.bind(this)}></img>
-    </div>)
+    var checkDisplay = this.props.highlight ? "initial" : "none";
+    return (
+    <div className="styleThumbnails-parent">
+      <img className="styleThumbnails-check" src="img/check-icon.png" style={{"display": checkDisplay}}/>
+      <div className="styleThumbnails-container" style={this.props.highlight ? highlightStyle : nonHighlightStyl}>
+        <img className="styleThumbnails" src={this.props.styleObj.photos[0].thumbnail_url || 'img/NoImageThumbnail.png'} onClick={this.handleMouseOver.bind(this)}></img>
+      </div>
+    </div>
+   )
 
   }
 }
